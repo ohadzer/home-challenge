957e07667ef91a2639e0e988e83f8da8eb9fcb01
root <root@ip-172-31-74-87.ec2.internal>
2020-07-05 18:26:25 +0000

    add outputs

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,15 @@
 docker build -t ohadz/ohad-app .
+if [ $? -eq 0 ]; then
+    echo Build Completed Successfully
+else
+    echo Build Faild
+    exit 1
+fi
 kubectl apply -f ./py-deployment.yaml
+if [ $? -eq 0 ]; then
+    echo Deployment Completed Successfully
+else
+    echo Deployment Faild
+    exit 1
+fi
+echo Script Has Been Completed Successfully!
